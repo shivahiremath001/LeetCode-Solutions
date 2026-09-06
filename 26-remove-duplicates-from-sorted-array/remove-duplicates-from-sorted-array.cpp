4913b5ab746e6e5684104cf1a208f727937773b7
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-06 14:44:22 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (80.52%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
-
-        int i = 1;
-
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[i - 1]) {
-                nums[i] = nums[j];
-                i++;
+        int j = 1;
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] != nums[i - 1]){
+                nums[j] = nums[i];
+                j++;
             }
         }
-
-        return i;        
+        return j;
     }
 };
+
 
 // class Solution {
 // public:
