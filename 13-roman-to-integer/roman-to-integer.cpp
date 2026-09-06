9f8df3e15f0e361093e338f6f0cca7ace6252680
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-06 11:50:15 +0530

    Time: 3 ms (75.15%) | Memory: 13.1 MB (57.85%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char, int> mpp;
+        int n = s.length();
         mpp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
                {'C', 100}, {'D', 500}, {'M', 1000}};
         int num = 0;
-        for (int i = 0; i < s.length(); i++){
-            num += mpp[s[i]];
-            if (i > 0 && mpp[s[i]] > mpp[s[i - 1]]){
-                num -= 2 * mpp[s[i - 1]];
+        for (int i = 0; i < n; i++){
+            if (i < n - 1 && mpp[s[i]] < mpp[s[i + 1]]){
+                num -= mpp[s[i]];
+            } else {
+                num += mpp[s[i]];
             }
         }
         return num;
