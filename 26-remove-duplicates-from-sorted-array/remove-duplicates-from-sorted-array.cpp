67245c4ae0800f69a37f68121f7fda68954a88e0
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-06 14:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (18.45%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        set<int> numSet;
-
-        for (int i = n - 1; i >= 0; i--){
-            numSet.insert(nums[i]);
+        vector<int> arr;
+        arr.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] != nums[i - 1]) arr.push_back(nums[i]);
         }
-        int setSize = numSet.size();
-        
-        int i = 0;
-        for (auto it: numSet){
-            if (i == setSize) break;
-            nums[i] = it;
-            i++;
+        for (int i = 0; i < arr.size(); i++){
+            nums[i] = arr[i];
         }
-        return setSize;
+        return arr.size();
     }
 };
+
+
+// int removeDuplicates(vector<int>& nums) {
+//         int n = nums.size();
+//         set<int> numSet;
+
+//         for (int i = n - 1; i >= 0; i--){
+//             numSet.insert(nums[i]);
+//         }
+//         int setSize = numSet.size();
+        
+//         int i = 0;
+//         for (auto it: numSet){
+//             if (i == setSize) break;
+//             nums[i] = it;
+//             i++;
+//         }
+//         return setSize;
