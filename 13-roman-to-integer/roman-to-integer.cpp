8a4d8c14acbdf3d31a923eaa6e17b7e8f6cd94b6
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-06 11:56:43 +0530

    Time: 7 ms (51.50%) | Memory: 13.1 MB (57.52%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -6,13 +6,13 @@ public:
         mpp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
                {'C', 100}, {'D', 500}, {'M', 1000}};
         int num = 0;
-        for (int i = 0; i < n-1; i++){
-            if (mpp[s[i]] < mpp[s[i + 1]]){
+        for (int i = 0; i < n; i++){
+            if (i < n - 1 && mpp[s[i]] < mpp[s[i + 1]]){
                 num -= mpp[s[i]];
             } else {
                 num += mpp[s[i]];
             }
         }
-        return num + mpp[s[n-1]];
+        return num;
     }
 };
