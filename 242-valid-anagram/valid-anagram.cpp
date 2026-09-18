3db42d041988bd90d563cb78f0486d418769b53b
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-18 23:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        if (s.size() != t.size()) return false; 
+        int arr[256] = {0};
+        for (char ch: s){
+            arr[ch]++;
+        }
+        for (char ch: t){
+            if (--arr[ch] < 0) return false;
+        }
+        return true;
     }
 };
