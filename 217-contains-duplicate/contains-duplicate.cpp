1e392ca499328cae446e60768dece1e16cdf025f
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-18 23:08:31 +0530

    Time: 28 ms (84.90%) | Memory: 82.7 MB (89.17%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++){
-            if (nums[i] == nums[i+1]) return true;
+
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i-1] == nums[i]) return true;
         }
         return false;
     }
