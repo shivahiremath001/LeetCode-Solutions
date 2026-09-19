d4c97e04a542d59e4b575c19ca58cdf20a0023ca
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-20 00:27:21 +0530

    Time: 14 ms (74.07%) | Memory: 26.1 MB (53.69%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false; 
-        int arr[26] = {0};
-        for (char ch: s) arr[ch - 'a']++;
-        for (char ch: t) if (--arr[ch - 'a'] < 0) return false;
-        return true;
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> res;
-        int n = strs.size();
-        vector<bool> arr(n, 1);
-
-        for (int i = 0; i < n; i++){
-            if (!arr[i]) continue;
-            vector<string> temp;
-            for (int j = i; j < n; j++){
-                if (isAnagram(strs[i], strs[j]) && arr[j]) {
-                    temp.push_back(strs[j]);
-                    arr[j] = 0;
-                }
-            }
-            res.push_back(temp);
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
         }
-        return res;
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.size() != t.size()) return false; 
+//         int arr[26] = {0};
+//         for (char ch: s) arr[ch - 'a']++;
+//         for (char ch: t) if (--arr[ch - 'a'] < 0) return false;
+//         return true;
+//     }
+
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         vector<vector<string>> res;
+//         int n = strs.size();
+//         vector<bool> arr(n, 1);
+
+//         for (int i = 0; i < n; i++){
+//             if (!arr[i]) continue;
+//             vector<string> temp;
+//             for (int j = i; j < n; j++){
+//                 if (isAnagram(strs[i], strs[j]) && arr[j]) {
+//                     temp.push_back(strs[j]);
+//                     arr[j] = 0;
+//                 }
+//             }
+//             res.push_back(temp);
+//         }
+//         return res;
+//     }
+// };
