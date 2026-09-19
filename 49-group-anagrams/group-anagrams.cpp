861d957eb8591953e3e65148c997c362ef00d9ba
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-20 00:29:16 +0530

    Time: 29 ms (20.63%) | Memory: 27.3 MB (22.19%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,9 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
         for (string s : strs) {
-            string t = s; 
-            sort(t.begin(), t.end());
-            mp[t].push_back(s);
+            mp[strSort(s)].push_back(s);
         }
         vector<vector<string>> anagrams;
         for (auto p : mp) { 
@@ -13,7 +11,37 @@ public:
         }
         return anagrams;
     }
+private:
+    string strSort(string s) {
+        int counter[26] = {0};
+        for (char c : s) {
+            counter[c - 'a']++;
+        }
+        string t;
+        for (int c = 0; c < 26; c++) {
+            t += string(counter[c], c + 'a');
+        }
+        return t;
+    }
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         unordered_map<string, vector<string>> mp;
+//         for (string s : strs) {
+//             string t = s; 
+//             sort(t.begin(), t.end());
+//             mp[t].push_back(s);
+//         }
+//         vector<vector<string>> anagrams;
+//         for (auto p : mp) { 
+//             anagrams.push_back(p.second);
+//         }
+//         return anagrams;
+//     }
+// };
 
 
 
