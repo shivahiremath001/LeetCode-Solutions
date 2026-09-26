426f47c4a9e511099ab58caf3aa3f5b12bd84c2a
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-09-26 20:42:20 +0530

    Time: 7 ms (15.40%) | Memory: 18.2 MB (34.18%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
-        map<int, int> mpp;
-        for (int i: nums) {
-            if (!mpp.count(i)) mpp[i] = 1;
-            else mpp[i]++;
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        vector<int>ans;
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mpp[nums[i]]++;
         }
-        vector<pair<int, int>> arr(mpp.size());
-        int z = 0;
-        for (auto i: mpp) {
-            arr[z].first = i.second;
-            arr[z].second = i.first;
-            z++;
+        priority_queue<pair<int,int>>pq;
+        for(auto it:mpp)
+        {
+            pq.push({it.second,it.first});
         }
-        sort(arr.rbegin(), arr.rend());
-        
-        vector<int> res(k);
-        for (int i = 0; i <= k - 1; i++){
-            res[i] = arr[i].second;
+        while(k>0)
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+            k--;
         }
-
-        return res;
+        return ans;
     }
+//please upvote...
 };
